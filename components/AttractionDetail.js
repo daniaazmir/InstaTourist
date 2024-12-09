8927837405fcbdb10abc0c1562d43aba5b339211
daniaazmir <daniaazmir@gmail.com>
2024-12-09 23:36:27 +0800

    updated ip in attractiondata and locationutils

--- components/AttractionDetail.js
+++ components/AttractionDetail.js
@@ -23,7 +23,7 @@ const AttractionDetail = ({ attraction, visible, onClose }) => {
   const fetchPlaceDetails = async () => {
     try {
       const response = await fetch(
-        `http://192.168.1.9:5000/api/place-details/${attraction.id}`
+        `http://192.168.1.8:5000/api/place-details/${attraction.id}`
       );
       const data = await response.json();
       setPlaceDetails(data);
--- utils/locationUtils.js
+++ utils/locationUtils.js
@@ -2,7 +2,7 @@ export const fetchNearbyAttractions = async (coords, radius) => {
   const { latitude, longitude } = coords;
   
   try {
-    const url = `http://192.168.1.9:5000/api/nearby-attractions/${latitude}/${longitude}/${radius}`;
+    const url = `http://192.168.1.8:5000/api/nearby-attractions/${latitude}/${longitude}/${radius}`;
     console.log('Fetching from URL:', url);
 
     const response = await fetch(url);
